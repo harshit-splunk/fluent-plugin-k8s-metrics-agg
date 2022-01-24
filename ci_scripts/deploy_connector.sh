6a3d06a9b2be3476381ea34f8507bbf40f4b4937
harshit-splunk <89519921+harshit-splunk@users.noreply.github.com>
2022-01-24 11:26:27 +0530

    Update deploy_connector.sh

--- ci_scripts/deploy_connector.sh
+++ ci_scripts/deploy_connector.sh
@@ -12,8 +12,8 @@ fi
 # Clone splunk-connect-for-kubernetes repo
 cd /opt
 git clone https://github.com/splunk/splunk-connect-for-kubernetes.git
-git checkout functional_test
 cd splunk-connect-for-kubernetes
+git checkout functional_test
 
 minikube image load splunk/k8s-metrics-aggr:recent
 
